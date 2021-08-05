ed01bb48f2b0a292df94541b18c5e7bdf6dd8cfa
Martin Chudoba <martin.chudoba@seznam.cz>
2021-08-05 13:28:11 +0200

    fix cpu refresh

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,14 +1,14 @@
 <template>
-    <div>
-        <div class="row">
-            <Revenue title="CPU usage" :value="cpu" />
-            <Memory title="Memory usage" :value="mem" id="memory" />
-            <Disk title="Disk usage" :value="disksize" id="disksize" />
-        </div>
-        <div class="row">
-            <RealTime title="Network (received kbytes / second)" :value="rx_sec" :has-percentage="true" />
-        </div>
+  <div>
+    <div class="row">
+      <Revenue title="CPU usage" :value="cpu"/>
+      <Memory title="Memory usage" :value="mem" id="memory"/>
+      <Disk title="Disk usage" :value="disksize" id="disksize"/>
     </div>
+    <div class="row">
+      <RealTime title="Network (received kbytes / second)" :value="rx_sec" :has-percentage="true"/>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -18,37 +18,39 @@ import Disk from './dashboard/Download.vue';
 import RealTime from './dashboard/RealTime.vue';
 
 export default {
-    name: 'dashboard',
-    components: {
-        Revenue,
-        Memory,
-        RealTime,
-        Disk
+  name: 'dashboard',
+  components: {
+    Revenue,
+    Memory,
+    RealTime,
+    Disk
+  },
+  data() {
+    return {
+      cpu: 0,
+      mem: 0,
+      rx_sec: 0,
+      disksize: 0
+    }
+  },
+  sockets: {
+    messageCPU(data) {
+      if (data.currentLoadSystem !== undefined) {
+        this.cpu = data.currentLoadSystem;
+        this.$emit('notification', this.cpu);
+      }
     },
-    data() {
-        return {
-            cpu: 0,
-            mem: 0,
-            rx_sec: 0,
-            disksize: 0
-        }
+    messageMemory(data) {
+      this.mem = (data.active / data.total) * 100;
     },
-    sockets: {
-        messageCPU(data) {
-            this.cpu = data.currentLoadSystem;
-            this.$emit('notification', this.cpu);
-        },
-        messageMemory(data) {
-            this.mem = (data.active / data.total) * 100;
-        },
-        messageNetwork(data) {
-            // console.log(data);
-            this.rx_sec = data.rx_sec / 1024; // kilo bytes
-        },
-        messageDiskspace(data) {
-            this.disksize = data.use; // used in %
-        }
+    messageNetwork(data) {
+      // console.log(data);
+      this.rx_sec = data.rx_sec / 1024; // kilo bytes
     },
+    messageDiskspace(data) {
+      this.disksize = data.use; // used in %
+    }
+  },
 }
 
 </script>
